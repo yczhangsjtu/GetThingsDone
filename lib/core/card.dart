75180ffb2689e8520727d400277aa6e58d9124d8
Yuncong <yczhangsjtu@gmail.com>
2019-07-11 08:20:24 +0800

    Implement Card and subclasses.

--- lib/core/card.dart
+++ lib/core/card.dart
@@ -1,6 +1,96 @@
+import 'time.dart';
+
 class Card {
   final int id;
   final String title;
   final List<String> comments;
-  Card(this.id, this.title, this.comments);
+
+  Card(this.id, this.title, {this.comments});
+
+  String toString() {
+    return "$title${comments?.isEmpty ?? true ? "" : "\n" + comments.join("\n")}";
+  }
 }
+
+enum Importance {
+  extreme,
+  high,
+  considerable,
+  normal,
+  none,
+}
+
+class CardUtils {
+  static importanceToString(Importance importance) {
+    if (importance == Importance.extreme) {
+      return "极重要";
+    }
+    if (importance == Importance.high) {
+      return "很重要";
+    }
+    if (importance == Importance.considerable) {
+      return "重要";
+    }
+    if (importance == Importance.normal) {
+      return "一般";
+    }
+    if (importance == Importance.none) {
+      return "不重要";
+    }
+    return null;
+  }
+}
+
+class ActionCard extends Card {
+  final List<TimeOption> timeOptions;
+  final String nextAct;
+  final Importance importance;
+  final String waiting;
+
+  ActionCard(
+    int id,
+    String title, {
+    this.timeOptions,
+    List<String> comments,
+    this.nextAct,
+    this.importance = Importance.normal,
+    this.waiting,
+  })  : assert(timeOptions?.isNotEmpty ?? false),
+        assert(!timeOptions.any((o) {
+          return o == null;
+        })),
+        assert(importance != null),
+        super(id, title, comments: comments);
+
+  @override
+  String toString() {
+    StringBuffer stringBuffer = StringBuffer();
+    stringBuffer.write(title);
+    stringBuffer
+        .write("\n${timeOptions.map((o) => o.toString()).toList().join("\n")}");
+    if (nextAct != null) {
+      stringBuffer.write("\n$nextAct");
+    }
+    if (importance != Importance.normal) {
+      stringBuffer.write("\n${CardUtils.importanceToString(importance)}");
+    }
+    if (waiting != null) {
+      stringBuffer.write("\n$waiting");
+    }
+    if (comments?.isNotEmpty ?? false) {
+      stringBuffer.write("\n${comments.join("\n")}");
+    }
+    return stringBuffer.toString();
+  }
+}
+
+class InventoryCard extends Card {
+  InventoryCard(int id, String title, {List<String> comments})
+      : super(id, title, comments: comments);
+}
+
+class BasketCard extends Card {
+  BasketCard(int id, String title, {List<String> comments})
+      : super(id, title, comments: comments);
+}
+
--- test/card_test.dart
+++ test/card_test.dart
@@ -1,7 +1,24 @@
 import 'package:flutter_test/flutter_test.dart';
 
+import 'package:flutter_gtd/core/time.dart';
+import 'package:flutter_gtd/core/date_time_utils.dart';
+import 'package:flutter_gtd/core/card.dart';
+
 void main() {
   test("Test card creation and toString", () {
+    // Test Card toString
+    expect(Card(0, "看书《白鹿原》").toString(), "看书《白鹿原》");
+    expect(Card(0, "看书《白鹿原》", comments: []).toString(), "看书《白鹿原》");
+    expect(Card(1, "洗衣服", comments: ["周日"]).toString(), "洗衣服\n周日");
+    expect(
+        Card(2, "洗澡", comments: ["周日下午6点", "重要"]).toString(), "洗澡\n周日下午6点\n重要");
 
+    // Test ActionCard toString
+    expect(
+        ActionCard(0, "看书《白鹿原》", timeOptions: [
+          FixedTime(DateTimeUtils.yearMonthDayToInt(2019, 7, 11),
+              start: 17 * 60)
+        ]).toString(),
+        "看书《白鹿原》\n2019-7-11 17:00");
   });
 }
